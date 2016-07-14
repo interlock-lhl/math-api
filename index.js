fc27176a747f99fb7fee56729cf10ae4e5dd6ed7
James Sapara <james.sapara@gmail.com>
2016-07-14 07:51:08 -0700

    first

---
+++ index.js
@@ -0,0 +1,7 @@
+var app = require('./lib/server');
+
+const PORT = process.env['PORT'] || 3000;
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port: ' + PORT);
+});
---
+++ test/test.js
@@ -0,0 +1,22 @@
+var chai = require('chai');
+var expect = chai.expect;
+var chaiHttp = require('chai-http')
+chai.use(chaiHttp);
+var app = require('../lib/server');
+
+describe('Indgiex Route', function() {
+  it ('Normal as can be', function(done) {
+    chai.request(app)
+      .get('/')
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.text).be.equal("Math API");
+        done();
+      });
+  });
+
+  describe("Addition Route", function() {
+
+  });
+});
