4e6c7f9727603fb84199be3e8c1e5f74b1098af1
James Sapara <james.sapara@gmail.com>
2016-08-08 07:57:39 -0700

    update to full suite of tests

--- test/test.js
+++ test/test.js
@@ -31,15 +31,51 @@ describe('Indgiex Route', function() {
   });
 
   describe("Sub Route", function() {
-
+    it("returns answer for valid numbers", function(done) {
+      chai.request(app)
+        .get('/sub/1/2')
+        .end(function(err, res) {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.text).be.equal("-1");
+          done();
+        });
+    });
   });
 
   describe("Multiplication Route", function() {
-
+    it("returns answer for valid numbers", function(done) {
+      chai.request(app)
+        .get('/mult/3/2')
+        .end(function(err, res) {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.text).be.equal("6");
+          done();
+        });
+    });
   });
 
   describe("Division Route", function() {
+    it("returns answer for valid numbers", function(done) {
+      chai.request(app)
+        .get('/div/0/2')
+        .end(function(err, res) {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.text).be.equal("0");
+          done();
+        });
+    });
 
+    it("returns error for invalid div by zero", function(done) {
+      chai.request(app)
+        .get('/div/3/0')
+        .end(function(err, res) {
+          expect(res).to.have.status(500);
+          done();
+        });
+    });
   });
 
 });
